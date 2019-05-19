dc36c3872f5161ca2e9ad7dc1d5b0924cd23e6c3
Merav <merav.mi@gmail.com>
2019-05-19 14:48:35 +0300

    added project files

---
+++ script.js
@@ -0,0 +1,10 @@
+< script >
+    function myFunction() {
+        var x = document.getElementById("myMenu");
+        if (x.className === "menu") {
+            x.className += " responsive";
+        } else {
+            x.className = "menu";
+        }
+    } <
+    /script>
